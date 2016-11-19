0e91c149d9f10503ceeccf394f86b64aecc94b74
Christian Gimber <cgimber@ucla.edu>
2016-11-19 15:29:08 -0800

    mobile web app support and cleanup

--- js/display.js
+++ js/display.js
@@ -5,13 +5,14 @@ var gifUrl;
 /* document ready
 ---------------------------------------------------------------------*/
 $(document).ready(function() {
-    getCat();
+    // hideAddressBar();
+    displayCat();
 });
 
 /* functions
 ---------------------------------------------------------------------*/
 
-function getCat() {
+function displayCat() {
     $.getJSON("http://api.giphy.com/v1/gifs/random?api_key=CW27AW0nlp5u0&tag=cat&rating=r", null, function(response) {
             var id = response.data.id;
             var image = response.data.image_original_url;
@@ -31,3 +32,14 @@ function getCat() {
             console.log("finished");
         });
 }
+
+// function hideAddressBar() {
+//     if (!window.location.hash) {
+//         if (document.height < window.outerHeight)
+//             document.body.style.height = (window.outerHeight + 50) + 'px';
+//         setTimeout(function() {
+//             window.scrollTo(0, 1);
+//             document.body.style.height = 'auto';
+//         }, 50);
+//     }
+// }
